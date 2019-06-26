c31bda35c172fb4fc1a8a3697b4491a9fde00b45
Igor Loginov <igor.loginov@ix.co>
2019-06-26 16:17:45 +0300

    templates are now with .j2 extension. kitchen idempotency tests added. sites variable can now be undefined. ansible_managed added to all templates. mime.types are now managed by ansible too.

--- tests/spec/nginx/nginx_spec.rb
+++ tests/spec/nginx/nginx_spec.rb
@@ -7,10 +7,41 @@ conf = YAML.load_file('../defaults/main.yml')
 sys = YAML.load_file('../vars/redhat.yml')
 
 port = '80'
-services = sys["nginx"]["services"]
+nginx_conf = '/etc/nginx/nginx.conf'
+mime_types = '/etc/nginx/mime.types'
+confdir = sys['nginx']['conf_dir']
+services = sys['nginx']['services']
 
 describe port(port) do
   it { should be_listening }
+end
+
+describe file(confdir) do
+  it { should exist }
+  it { should be_directory }
+end
+
+describe file(confdir + 'localhost.conf') do
+  it { should exist }
+  its(:content) { should match /Ansible/ }
+  its(:content) { should match /stub_status/ }
+end
+
+describe file(confdir + 'test.conf') do
+  it { should exist }
+  its(:content) { should match /Ansible/ }
+  its(:content) { should match /test.example.com/ }
+  its(:content) { should match /stub_status/ }
+end
+
+describe file(nginx_conf) do
+  it { should exist }
+  its(:content) { should match /Ansible/ }
+end
+
+describe file(mime_types) do
+  it { should exist }
+  its(:content) { should match /Ansible/ }
 end
 
 services.each do |service|
@@ -21,5 +52,5 @@ services.each do |service|
 end
 
 describe user('nginx') do
-  it { should have_login_shell conf["nginx_conf"]["user_shell"]}
+  it { should have_login_shell conf['nginx_conf']['user_shell']}
 end
