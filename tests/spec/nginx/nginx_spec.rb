693e0ef8e421f3038b88fec130decdf466ebdcf7
Igor Loginov <igor.loginov@ix.co>
2019-06-26 16:32:36 +0300

    tests added. conf dir managed by ansible.

--- tests/spec/nginx/nginx_spec.rb
+++ tests/spec/nginx/nginx_spec.rb
@@ -7,8 +7,10 @@ conf = YAML.load_file('../defaults/main.yml')
 sys = YAML.load_file('../vars/redhat.yml')
 
 port = '80'
+nginx_user = conf['nginx_conf']['user']
 nginx_conf = '/etc/nginx/nginx.conf'
 mime_types = '/etc/nginx/mime.types'
+pidfile = conf['nginx_conf']['pidfile']
 confdir = sys['nginx']['conf_dir']
 services = sys['nginx']['services']
 
@@ -16,15 +18,21 @@ describe port(port) do
   it { should be_listening }
 end
 
+describe file(pidfile) do
+  it { should exist }
+end
+
 describe file(confdir) do
   it { should exist }
   it { should be_directory }
+  it { should be_owned_by nginx_user }
 end
 
 describe file(confdir + 'localhost.conf') do
   it { should exist }
   its(:content) { should match /Ansible/ }
   its(:content) { should match /stub_status/ }
+  it { should be_owned_by nginx_user }
 end
 
 describe file(confdir + 'test.conf') do
@@ -32,16 +40,19 @@ describe file(confdir + 'test.conf') do
   its(:content) { should match /Ansible/ }
   its(:content) { should match /test.example.com/ }
   its(:content) { should match /stub_status/ }
+  it { should be_owned_by nginx_user }
 end
 
 describe file(nginx_conf) do
   it { should exist }
   its(:content) { should match /Ansible/ }
+  it { should be_owned_by nginx_user }
 end
 
 describe file(mime_types) do
   it { should exist }
   its(:content) { should match /Ansible/ }
+  it { should be_owned_by nginx_user }
 end
 
 services.each do |service|
